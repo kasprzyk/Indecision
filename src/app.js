e746bba31500e163fbf6700ed86419a41fd8c745
kasprzyk <piotkas1@gmail.com>
2020-08-31 17:43:05 +0200

    Exploring jsx

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 console.log('app is running');
 
 //jsx
-var template = <p>JSX from app.js</p>;
+var template = (
+  <div>
+    <h1>Indecision app</h1> <p>test</p>
+    <ol>
+      <li>Item</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Piotr Kasprzycki</h1>
+    <p>Age 32</p>
+  </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

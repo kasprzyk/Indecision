56b4037af3213a736deafd5f9164c4610593b720
kasprzyk <piotkas1@gmail.com>
2020-09-01 17:36:20 +0200

    conditional rendering in jsx

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 console.log('app is running');
 
 //jsx
+var app = {
+  title: 'test',
+  subtitle: 'test',
+};
 var template = (
   <div>
-    <h1>Indecision app</h1> <p>test</p>
+    <h1>{app.title}</h1> <p>test</p>
     <ol>
       <li>Item</li>
     </ol>
@@ -12,13 +16,23 @@ var template = (
 var user = {
   name: 'Piotr',
   age: 27,
+  location: 'Ilawa',
 };
 var userName = 'Piotr';
 var userAge = 27;
+
+function getLocation(location) {
+  if (location) {
+    return <p>{location}</p>;
+  } else {
+    return 'unknown';
+  }
+}
 var templateTwo = (
   <div>
     <h1>{user.name.toUpperCase() + '!'}</h1>
     <p>Age: {user.age}</p>
+    {getLocation(user.location)}
   </div>
 );
 var appRoot = document.getElementById('app');

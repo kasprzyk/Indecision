1e6e7bbda796a841cf8b462fd9d9770d3cb01d56
kasprzyk <piotkas1@gmail.com>
2020-09-09 17:10:21 +0200

    Events, methods

--- src/app.js
+++ src/app.js
@@ -26,19 +26,25 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {}
+
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('test');
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove all</button>
         {this.props.options.map((option) => (
           <Option key={option} optionText={option} />
         ))}
@@ -53,8 +59,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
-    return <div>AddOption component</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type='text' name='option'></input>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 

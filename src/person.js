415efe6f1079a2ce8b7b1f60118969e8c87b5a52
kasprzyk <piotkas1@gmail.com>
2020-09-14 16:08:26 +0200

    ES6 import/export

---
+++ src/person.js
@@ -0,0 +1,9 @@
+const isAdult = (age) => {
+  return age >= 18;
+};
+
+const canDrink = (age) => {
+  return age >= 21;
+};
+
+export { canDrink, isAdult };
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+console.log('test');
+
+const square = (x) => {
+  return x * x;
+};
+
+const add = (a, b) => {
+  return a + b;
+};
+
+export { square, add };

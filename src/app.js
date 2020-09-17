6d70a2d97115592da20a26f8babf2dbe520cd6e8
kasprzyk <piotkas1@gmail.com>
2020-09-17 11:00:20 +0200

    Setting up react modal

--- src/app.js
+++ src/app.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-const layout = (props) => {
-  return (
-    <div>
-      <p>header</p>
-      {props.content}
-      <p>footer</p>
-    </div>
-  );
-};
+// const layout = (props) => {
+//   return (
+//     <div>
+//       <p>header</p>
+//       {props.content}
+//       <p>footer</p>
+//     </div>
+//   );
+// };
 
-const template = (
-  <div>
-    <h1>Page title</h1>
-    <p>This is my page</p>
-  </div>
-);
+// const template = (
+//   <div>
+//     <h1>Page title</h1>
+//     <p>This is my page</p>
+//   </div>
+// );
 
 ReactDOM.render(
   <IndecisionApp options={['one', 'two']} />,
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Action from './Action';
 import Header from './Header';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
   state = {
     options: [],
+    selectedOption: undefined,
   };
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
@@ -19,7 +21,9 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({
+      selectedOption: option,
+    }));
   };
   handleAddOption = (option) => {
     if (!option) {
@@ -69,6 +73,7 @@ export default class IndecisionApp extends React.Component {
           handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
+        <OptionModal selectedOption={this.state.selectedOption} />
       </div>
     );
   }

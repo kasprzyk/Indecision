e0fb9d4df457c6bb01b771a4d40a62019da95a4d
kasprzyk <piotkas1@gmail.com>
2020-09-17 20:32:42 +0200

    normalize added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
 // const layout = (props) => {
 //   return (
 //     <div>
